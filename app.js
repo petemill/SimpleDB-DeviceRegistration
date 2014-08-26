5965033e73bab1ff026862f7f0bb17179140322b
Pete Miller <miller.pete@gmail.com>
2014-08-26 19:19:44 +0100

    Structure of V1 through comments

---
+++ app.js
@@ -0,0 +1,7 @@
+#read aws keys from disk
+
+#read 'friendly' group ID of this device from disk
+
+#retreive IP address and MAC address of current device
+
+#update simpledb row with key of MAC address all the above and the current dates
